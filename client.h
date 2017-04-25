ac1153cfc6a413c55edbe4cec3ebe9a408d5bf57
Akash Trehan <akash.trehan123@gmail.com>
2017-04-25 20:56:36 +0530

    Add client.h

---
+++ client.h
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+#include <signal.h> // For caputuring ctrl+c and freeing the port
+#include <functional> // for std::bind
+
+class chat
+{
+    struct history
+    {
+        struct message // Stores a messages and corresponding reply
+        {
+            std::string msg;
+            bool mine; // = 1 if message is mine else 0
+        };
+
+        struct frnd // History of one friend
+        {
+            std::string username;
+            std::string name;
+            std::vector<message> messages;
+        };
+
+        std::unordered_map<std::string, frnd> user_to_frnd; // Convert username to index for faster access of history
+
+
+    };
+
+public:
+    std::vector<std::string> online; // Vector containing username of online friends
+    std::vector<std::string> all; // Vector containing list of all friends
+    history hist; // Contains history of current session
+    std::string username; // Username of client
+    std::string name; // Name of client
+    int portno; // Port Number of the server
+    bool loggedin; // Is client logged in?
+    int sockfd; //  The socket over which connection is running
+
+    static const char escape_char = '\\'; // Escape character for escaping separators in client messages
+
+    void initialise_online(std::string msg); // Initially add all people who are online
+    void update_online(std::string msg); // Add a person if he comes online
+    void initialise_all(std::string msg); // Add all people part of the network
+    void update_all(std::string msg); // Add if a new person joins the network
+
+    void connect_to_server(char* server_name, int port_num);
+    void start_chat();
+
+    void write_thread();
+
+    void write_helper(char buffer[]);
+    void write_helper(std::string str_buffer);
+
+    void read_thread();
+
+    void free_port(int s);
+    void signal_capture();
+
+    // Helper functions
+    static void error(const char *msg);
+    static void print_help();
+    static std::vector<std::string> break_string(std::string msg);
+};
