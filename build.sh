3ec11e2dea39f22a7e6e3b6e423defea89162be5
Akash Trehan <akash.trehan123@gmail.com>
2017-04-29 03:30:59 +0530

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-g++ -std=c++11 -g -pthread -lsodium -lsqlite3 -lldap -Wno-deprecated-declarations server.cpp -o server
+g++ -std=c++11 -g -Wno-deprecated-declarations server.cpp -o server -pthread -lsodium -lsqlite3 -lldap
 g++ -std=c++11 -g -pthread -lsqlite3 client.cpp -o client
